014162ba7cb48f3583b311cc2021d7b33e68d2e8
Luis Angel <angel.quispe.navarro@outlook.com>
2025-03-31 20:01:28 -0500

    Primer Commit: Configuracion base del proyecto backend

---
+++ src/test/java/com/miempresa/busesapi/BusesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miempresa.busesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
