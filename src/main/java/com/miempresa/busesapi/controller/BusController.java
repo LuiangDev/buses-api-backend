fcf5d7c478dba3b64b716ca3d3e4d68a043bc077
Luis Angel <angel.quispe.navarro@outlook.com>
2025-04-01 08:31:07 -0500

    Habilitacion de CORS para la ruta de prueba

--- src/main/java/com/miempresa/busesapi/controller/BusController.java
+++ src/main/java/com/miempresa/busesapi/controller/BusController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/bus")
+@CrossOrigin(origins = "http://localhost:5173") // Habilita CORS para el origen especificado
 public class BusController {
 
     @Autowired
